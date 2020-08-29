5cfbbf54bd5842818e8aeade69d7fc33da55bd43
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:24:01 -0300

    Fix identation

--- src/shared/App.js
+++ src/shared/App.js
@@ -5,22 +5,22 @@ import NoMatch from "./NoMatch";
 import NavBar from "./NavBar";
 
 const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        {routes.map(({ path, exact, component: C, ...rest }) => (
-          <Route
-            key={path}
-            path={path}
-            exact={exact}
-            render={(props) => <C {...props} {...rest} />}
-          />
-        ))}
-        <Route render={(props) => <NoMatch {...props} />} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Switch>
+				{routes.map(({ path, exact, component: C, ...rest }) => (
+					<Route
+						key={path}
+						path={path}
+						exact={exact}
+						render={(props) => <C {...props} {...rest} />}
+					/>
+				))}
+				<Route render={(props) => <NoMatch {...props} />} />
+			</Switch>
+		</div>
+	);
 };
 
 export default App;
