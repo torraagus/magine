3a7bcdf58484af29a4ce1361d3f17132a4b5a812
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:27:39 -0300

    Update Home page

--- src/shared/pages/home/Home.js
+++ src/shared/pages/home/Home.js
@@ -1,49 +1,73 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { fetchMovies } from "../../api";
-import { Category, Image, Movie, Movies, Title } from "./home.styles";
+import { withRouter } from "react-router-dom";
+import {
+	Category,
+	CategoryTitle,
+	Image,
+	Movie,
+	Movies,
+	Title,
+	MovieTitle,
+	Wrapper,
+	SeeAllBtn,
+	VoteAverage,
+	ReleaseDate,
+	ReleaseDateText,
+} from "./home.styles";
+import { useFetch } from "../../../browser/hooks/useFetch";
 
-const Home = (props) => {
-	const [movies, setMovies] = useState([]);
-	const titles = ["Now playing", "Popular", "Top rated", "Upcoming"];
-
-	useEffect(() => {
-		if (__isBrowser__) {
-			console.log("browser");
-			setMovies(window.__INITIAL_DATA__);
-			delete window.__INITIAL_DATA__;
-		} else {
-			console.log("server");
-			setMovies(props.staticContext);
-		}
-	}, []);
-
-	// useEffect(() => {
-	// 	(async () => {
-	// 		const movies = await fetchNowPlayingMovies("en:US");
-	// 		setMovies(movies);
-	// 	})();
-	// }, [movies]);
-
-	return (
-		<>
-			{titles.map((title, index) => (
-				<Fragment key={title}>
-					<Category>{title}</Category>
-					<Movies>
-						{movies[index]?.map((movie) => (
-							<Movie key={movie.id}>
-								<Image
-									style={{ height: 300 }}
-									src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-								/>
-								<Title>{movie.title}</Title>
-							</Movie>
-						))}
-					</Movies>
-				</Fragment>
-			))}
-		</>
+const Home = ({ location, fetchInitialData, history }) => {
+	// const [movies, setMovies] = useState([]);
+	const categories = [
+		{ title: "Now playing", path: "/now-playing" },
+		{ title: "Popular", path: "/popular" },
+		{ title: "Top rated", path: "/top-rated" },
+		{ title: "Upcoming", path: "/upcoming" },
+	];
+	const { movies, loading } = useFetch(
+		location.pathname,
+		fetchInitialData,
+		true
 	);
+
+	const handleClick = (path) => {
+		history.push(`${path}/1`);
+	};
+
+	if (loading) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			<Wrapper>
+				<Title>Movies</Title>
+				{movies && console.log(movies[0])}
+				{categories.map((category, index) => (
+					<Fragment key={category.title}>
+						<Category>
+							<CategoryTitle>{category.title}</CategoryTitle>
+							<SeeAllBtn onClick={() => handleClick(category.path)}>
+								See all
+							</SeeAllBtn>
+						</Category>
+						<Movies>
+							{movies &&
+								movies[index]?.map((movie) => (
+									<Movie key={movie.id}>
+										<Image
+											src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+										/>
+										<MovieTitle>{movie.title}</MovieTitle>
+										<VoteAverage>{movie.vote_average}</VoteAverage>
+										<ReleaseDate>{movie.release_date}</ReleaseDate>
+										<ReleaseDateText>Release date</ReleaseDateText>
+									</Movie>
+								))}
+						</Movies>
+					</Fragment>
+				))}
+			</Wrapper>
+		);
+	}
 };
 
-export default Home;
+export default withRouter(Home);
