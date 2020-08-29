23f8c6fef5e1c8a58386e5e93f9ddb7b110ff0e2
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:29:54 -0300

    Update Categories page

---
+++ src/shared/pages/categories/Categories.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+	Pages,
+	Image,
+	Movie,
+	Movies,
+	Name,
+	Title,
+	Wrapper,
+	Category,
+	BackButton,
+	VoteAverage,
+	ReleaseDate,
+	ReleaseDateText,
+} from "./categories.styles";
+import { useFetch } from "../../../browser/hooks/useFetch";
+import Pagination from "../../components/pagination/Pagination";
+
+const AllMovies = ({ location, fetchInitialData, history, name, basePath }) => {
+	const { movies, page, totalPages, loading } = useFetch(
+		location.pathname,
+		fetchInitialData,
+		false
+	);
+
+	const handleOnPageChange = (page) => {
+		history.push(`${basePath}/${page}`);
+	};
+
+	return (
+		<Wrapper>
+			<BackButton size={36} onClick={() => history.push("/")}></BackButton>
+			<Category>
+				<Name>{name}</Name>
+				{page && (
+					<Pages>
+						Page {page} of {totalPages}
+					</Pages>
+				)}
+			</Category>
+			{loading && <Pages>Loading...</Pages>}
+			<Pagination
+				upper
+				page={page}
+				totalPages={totalPages}
+				onPageChange={handleOnPageChange}
+			/>
+			{!loading && (
+				<Movies>
+					{movies?.map((movie) => (
+						<Movie key={movie.id}>
+							<Image
+								src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+							/>
+							<Title>{movie.title}</Title>
+							<VoteAverage>{movie.vote_average}</VoteAverage>
+							<ReleaseDate>{movie.release_date}</ReleaseDate>
+							<ReleaseDateText>Release date</ReleaseDateText>
+						</Movie>
+					))}
+				</Movies>
+			)}
+			<Pagination
+				page={page}
+				totalPages={totalPages}
+				onPageChange={handleOnPageChange}
+			/>
+		</Wrapper>
+	);
+};
+
+export default AllMovies;
