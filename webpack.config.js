3da86bc1c2eee48f7913c8367bc24bfa7bf1d70d
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:09:34 -0300

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,53 @@
 const path = require("path"),
-  webpack = require("webpack"),
-  nodeExternals = require("webpack-node-externals");
+	webpack = require("webpack"),
+	nodeExternals = require("webpack-node-externals"),
+	MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const browserConfig = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "browser", "index.js"),
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [{ test: /\.(js)$/, use: "babel-loader" }],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __isBrowser__: "true",
-    }),
-  ],
+	mode: "development",
+	entry: path.resolve(__dirname, "src", "browser", "index.js"),
+	output: {
+		path: path.resolve(__dirname, "public"),
+		filename: "bundle.js",
+		publicPath: "/",
+	},
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: "babel-loader" },
+			{
+				test: /\.(css|sass|scss)$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+		],
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			__isBrowser__: "true",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "styles.css",
+		}),
+	],
 };
 
 const serverConfig = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "server", "index.js"),
-  target: "node",
-  externals: [nodeExternals()],
-  output: {
-    path: __dirname,
-    filename: "server.js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [{ test: /\.(js)$/, use: "babel-loader" }],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __isBrowser__: "false",
-    }),
-  ],
+	mode: "development",
+	entry: path.resolve(__dirname, "src", "server", "index.js"),
+	target: "node",
+	externals: [nodeExternals()],
+	output: {
+		path: __dirname,
+		filename: "server.js",
+		publicPath: "/",
+	},
+	module: {
+		rules: [{ test: /\.(js)$/, use: "babel-loader" }],
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			__isBrowser__: "false",
+		}),
+	],
 };
 
 module.exports = [browserConfig, serverConfig];
