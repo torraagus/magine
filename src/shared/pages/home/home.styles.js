410bbdfed5cb1e8a2d7b11fb074141cfe46611eb
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:28:10 -0300

    Update Home styles

--- src/shared/pages/home/home.styles.js
+++ src/shared/pages/home/home.styles.js
@@ -1,26 +1,50 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { colors } from "../../../browser/styles/colors";
+import {
+	PrimaryBtnSC,
+	MoviesWrapperSC,
+	MovieImageSC,
+	MovieSC,
+	MovieTitleSC,
+	ReleaseDateSC,
+	ReleaseDateTextSC,
+	VoteAverageSC,
+} from "../../styled-components/components";
 
-export const Category = styled.h2`
+export const Wrapper = styled(MoviesWrapperSC)``;
+
+export const Category = styled.div`
+	margin-top: 2rem;
+	display: flex;
+	align-items: center;
+`;
+export const CategoryTitle = styled.h2`
 	letter-spacing: 3px;
-	margin: 2rem 0 1rem 0.5rem;
+	margin: 0 0.5rem 0 0.5rem;
+	font-weight: 500;
 `;
 export const Movies = styled.ul`
 	display: flex;
 	list-style: none;
-	padding: 0;
+	padding: 0 0 1rem 0;
+	margin-left: 0.5rem;
+	overflow-x: auto;
 `;
-export const Movie = styled.li`
+export const Movie = styled(MovieSC)`
+	width: 200px;
 	margin: 0.5rem;
-	text-align: center;
+`;
+export const Image = styled(MovieImageSC)`
+	min-width: 200px;
+`;
+export const MovieTitle = styled(MovieTitleSC)``;
+export const Title = styled.h1`
+	margin: 4rem 0 0 0;
+	font-weight: 900;
+	letter-spacing: 5px;
+`;
+export const SeeAllBtn = styled(PrimaryBtnSC)``;
 
-	-webkit-box-shadow: 10px 10px 5px 0px #651a1b31;
-	-moz-box-shadow: 10px 10px 5px 0px #651a1b2f;
-	box-shadow: 5px 5px 5px 0px #651a1b27;
-`;
-export const Image = styled.img`
-	height: 300px;
-`;
-export const Title = styled.p`
-	font-weight: 16px;
-	letter-spacing: 2px;
-`;
+export const VoteAverage = styled(VoteAverageSC)``;
+export const ReleaseDate = styled(ReleaseDateSC)``;
+export const ReleaseDateText = styled(ReleaseDateTextSC)``;
