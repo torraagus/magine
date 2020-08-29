20546dece1c824bc006935e6ecffbba1404e2e45
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:15:12 -0300

    Update file

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ import { renderToString } from "react-dom/server";
 import App from "../shared/App";
 import React from "react";
 import serialize from "serialize-javascript";
-import { matchPath, StaticRouter } from "react-router-dom";
+import { matchPath, StaticRouter as Router } from "react-router-dom";
 import routes from "../shared/routes";
 
 const app = express();
@@ -12,28 +12,34 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 
+app.get("/favicon.ico", (req, res) => res.status(204));
+
 app.get("*", (req, res, next) => {
 	const activeRoute = routes.find((route) => matchPath(req.url, route)) || {};
 
 	const promise = activeRoute.fetchInitialData
-		? activeRoute.fetchInitialData()
+		? activeRoute.fetchInitialData(req.params[0].split("/").pop())
 		: Promise.resolve();
 
 	promise
 		.then((data) => {
 			const markup = renderToString(
-				<StaticRouter location={req.url} context={{ data }}>
+				<Router location={req.url} context={{ data }}>
 					<App />
-				</StaticRouter>
+				</Router>
 			);
 
 			res.send(`
 				<!DOCTYPE html>
-				<html>
+				<html lang="en">
 					<head>
+						<meta charset="UTF-8">
+						<meta name="viewport" content="width=device-width, initial-scale=1.0">
 						<title>SSR with RR</title>
 						<script src="/bundle.js" defer></script>
 						<script>window.__INITIAL_DATA__=${serialize(data)}</script>
+						<link rel="stylesheet" type="text/css" href="/styles.css">
+						<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;900&display=swap" rel="stylesheet">
 					</head>
 			
 					<body>
