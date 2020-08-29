6767856eb87c6bc2dc33905f7ec7a21d5bd1cf64
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:16:00 -0300

    Update api file

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,8 +1,7 @@
 import fetch from "isomorphic-fetch";
 
+const apiKey = "986ff47b5082746e703a6f7cb592b5b7";
 export const fetchMovies = () => {
-	const apiKey = "986ff47b5082746e703a6f7cb592b5b7";
-
 	let urls = [
 		`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`,
 		`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`,
@@ -15,6 +14,50 @@ export const fetchMovies = () => {
 	return Promise.all(requests)
 		.then((responses) => Promise.all(responses.map((r) => r.json())))
 		.then((data) => Promise.all(data.map((r) => r.results)))
+		.catch((err) => {
+			console.warn(err);
+			return null;
+		});
+};
+
+export const fetchNowPlayingMovies = (page) => {
+	let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}`;
+
+	return fetch(url)
+		.then((res) => res.json())
+		.catch((err) => {
+			console.warn(err);
+			return null;
+		});
+};
+
+export const fetchPopularMovies = (page) => {
+	let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
+
+	return fetch(url)
+		.then((res) => res.json())
+		.catch((err) => {
+			console.warn(err);
+			return null;
+		});
+};
+
+export const fetchTopRatedMovies = (page) => {
+	let url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=${page}`;
+
+	return fetch(url)
+		.then((res) => res.json())
+		.catch((err) => {
+			console.warn(err);
+			return null;
+		});
+};
+
+export const fetchUpcomingMovies = (page) => {
+	let url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=${page}`;
+
+	return fetch(url)
+		.then((res) => res.json())
 		.catch((err) => {
 			console.warn(err);
 			return null;
