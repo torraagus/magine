b62a93b65b0831fd6f080f64b741e70150c2c008
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 12:25:48 -0300

    Add routes to file

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,5 +1,12 @@
 import Home from "./pages/home/Home";
-import { fetchMovies } from "./api";
+import {
+	fetchMovies,
+	fetchNowPlayingMovies,
+	fetchPopularMovies,
+	fetchTopRatedMovies,
+	fetchUpcomingMovies,
+} from "./api";
+import Categories from "./pages/categories/Categories";
 
 const routes = [
 	{
@@ -7,6 +14,34 @@ const routes = [
 		exact: true,
 		component: Home,
 		fetchInitialData: () => fetchMovies(),
+	},
+	{
+		path: "/now-playing/:page",
+		basePath: "/now-playing",
+		component: Categories,
+		name: "Now playing",
+		fetchInitialData: (page) => fetchNowPlayingMovies(page),
+	},
+	{
+		path: "/popular/:page",
+		basePath: "/popular",
+		component: Categories,
+		name: "Popular",
+		fetchInitialData: (page) => fetchPopularMovies(page),
+	},
+	{
+		path: "/top-rated/:page",
+		basePath: "/top-rated",
+		component: Categories,
+		name: "Top rated",
+		fetchInitialData: (page) => fetchTopRatedMovies(page),
+	},
+	{
+		path: "/upcoming/:page",
+		basePath: "/upcoming",
+		component: Categories,
+		name: "Upcoming",
+		fetchInitialData: (page) => fetchUpcomingMovies(page),
 	},
 ];
 
