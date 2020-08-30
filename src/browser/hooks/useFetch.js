14653c90ae678e9b8a55fc8899f8d0607117b40d
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-29 21:49:44 -0300

    Change some var names

--- src/browser/hooks/useFetch.js
+++ src/browser/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (url, fetchMovies, fromHome) => {
+export const useFetch = (url, fetchMovies, isPaginated) => {
 	const [state, setState] = useState({
 		movies: [],
 		page: 1,
@@ -8,31 +8,31 @@ export const useFetch = (url, fetchMovies, fromHome) => {
 		loading: true,
 	});
 
-	const page = url.split("/").pop();
+	const param = url.split("/").pop();
 
 	useEffect(() => {
 		setState({ loading: true });
 		(async () => {
 			if (__isBrowser__ && window.__INITIAL_DATA__) {
 				setState({
-					page: fromHome
+					page: !isPaginated
 						? window.__INITIAL_DATA__
 						: window.__INITIAL_DATA__.page,
-					movies: fromHome
+					results: !isPaginated
 						? window.__INITIAL_DATA__
 						: window.__INITIAL_DATA__.results,
-					totalPages: fromHome
+					totalPages: !isPaginated
 						? window.__INITIAL_DATA__
 						: window.__INITIAL_DATA__.total_pages,
 					loading: false,
 				});
 				console.log(window.__INITIAL_DATA__, "use window");
 			} else {
-				const res = await fetchMovies(page);
+				const res = await fetchMovies(param);
 				setState({
-					page: fromHome ? res : res.page,
-					movies: fromHome ? res : res.results,
-					totalPages: fromHome ? res : res.total_pages,
+					page: !isPaginated ? res : res.page,
+					results: !isPaginated ? res : res.results,
+					totalPages: !isPaginated ? res : res.total_pages,
 					loading: false,
 				});
 				console.log(res, "use fetch");
