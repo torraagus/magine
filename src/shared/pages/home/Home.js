86a60b1ca29db64c4b913dba4435fb0a296e26ce
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-31 19:25:34 -0300

    Fix identation and extract the movie list to a component called MovieList

--- src/shared/pages/home/Home.js
+++ src/shared/pages/home/Home.js
@@ -1,27 +1,11 @@
 import React, { Fragment } from "react";
 import { withRouter } from "react-router-dom";
-import {
-	Category,
-	CategoryTitle,
-	Image,
-	Movie,
-	Movies,
-	Title,
-	MovieTitle,
-	Wrapper,
-	SeeAllBtn,
-	VoteAverage,
-	ReleaseDate,
-	ReleaseDateText,
-} from "./home.styles";
+import { Category, CategoryTitle, Movies, Title, Wrapper, SeeAllBtn } from "./home.styles";
 import { useFetch } from "../../../browser/hooks/useFetch";
+import MovieList from "../../components/movies/MovieList";
 
 const Home = ({ location, fetchInitialData, history, categories }) => {
-	const { results: movies, loading } = useFetch(
-		location.pathname,
-		fetchInitialData,
-		false
-	);
+	const { results: movies, loading } = useFetch(location.pathname, fetchInitialData, false);
 
 	const handleClick = (path) => {
 		history.push(path);
@@ -37,27 +21,9 @@ const Home = ({ location, fetchInitialData, history, categories }) => {
 					<Fragment key={category.title}>
 						<Category>
 							<CategoryTitle>{category.title}</CategoryTitle>
-							<SeeAllBtn onClick={() => handleClick(`${category.path}/1`)}>
-								See all
-							</SeeAllBtn>
+							<SeeAllBtn onClick={() => handleClick(`${category.path}/1`)}>See all</SeeAllBtn>
 						</Category>
-						<Movies>
-							{movies &&
-								movies[index]?.map((movie) => (
-									<Movie
-										key={movie.id}
-										onClick={() => handleClick(`/movies/id/${movie.id}`)}
-									>
-										<Image
-											src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-										/>
-										<MovieTitle>{movie.title}</MovieTitle>
-										<VoteAverage>{movie.vote_average}</VoteAverage>
-										<ReleaseDate>{movie.release_date}</ReleaseDate>
-										<ReleaseDateText>Release date</ReleaseDateText>
-									</Movie>
-								))}
-						</Movies>
+						<Movies>{movies && <MovieList movies={movies[index]} index={index} history={history} />}</Movies>
 					</Fragment>
 				))}
 			</Wrapper>
