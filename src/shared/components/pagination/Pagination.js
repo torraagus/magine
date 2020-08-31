e4a4f106811e3438269bc22b313bc003b16c4782
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-31 11:04:54 -0300

    Make return conditionally by totalPages prop

--- src/shared/components/pagination/Pagination.js
+++ src/shared/components/pagination/Pagination.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Wrapper, Next, Prev } from "./pagination.styles";
 
-const Pagination = ({page, totalPages, onPageChange, upper}) => {
-	return (
-		<Wrapper upper>
+const Pagination = ({ page, totalPages, onPageChange, upper }) => {
+	return totalPages > 1 ? (
+		<Wrapper upper={upper}>
 			{page > 1 && <Prev onClick={() => onPageChange(page - 1)}>Previous</Prev>}
 			{page < totalPages && (
 				<Next onClick={() => onPageChange(page + 1)}>Next</Next>
 			)}
 		</Wrapper>
-	);
+	) : null;
 };
 
 export default Pagination;
