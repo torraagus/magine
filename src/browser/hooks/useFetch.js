61d0fd682b3fd48ca2187a00cf253e8532640690
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-31 10:58:24 -0300

    [Refactor] Allow to pass multiple params to url in api.js

--- src/browser/hooks/useFetch.js
+++ src/browser/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (url, fetchMovies, isPaginated) => {
+export const useFetch = (url, fetch, isPaginated, params) => {
 	const [state, setState] = useState({
 		movies: [],
 		page: 1,
@@ -8,31 +8,37 @@ export const useFetch = (url, fetchMovies, isPaginated) => {
 		loading: true,
 	});
 
+	console.log(url, "in fetch");
 	const param = url.split("/").pop();
-
+	
 	useEffect(() => {
 		setState({ loading: true });
 		(async () => {
+			console.log(__isBrowser__, "is Browser in fetch");
+			console.log(window.__INITIAL_DATA__, "window obj in fetch");
 			if (__isBrowser__ && window.__INITIAL_DATA__) {
 				setState({
 					page: !isPaginated
-						? window.__INITIAL_DATA__
-						: window.__INITIAL_DATA__.page,
+					? window.__INITIAL_DATA__
+					: window.__INITIAL_DATA__.page,
 					results: !isPaginated
-						? window.__INITIAL_DATA__
-						: window.__INITIAL_DATA__.results,
+					? window.__INITIAL_DATA__
+					: window.__INITIAL_DATA__.results,
 					totalPages: !isPaginated
-						? window.__INITIAL_DATA__
-						: window.__INITIAL_DATA__.total_pages,
+					? window.__INITIAL_DATA__
+					: window.__INITIAL_DATA__.total_pages,
 					loading: false,
 				});
 				console.log(window.__INITIAL_DATA__, "use window");
+				delete window.__INITIAL_DATA__;
 			} else {
-				const res = await fetchMovies(param);
+				console.log(params, "params in fetch");
+				const res = params ? await fetch(params) : await fetch(param);
+
 				setState({
-					page: !isPaginated ? res : res.page,
-					results: !isPaginated ? res : res.results,
-					totalPages: !isPaginated ? res : res.total_pages,
+					page: !isPaginated ? res : res?.page,
+					results: !isPaginated ? res : res?.results,
+					totalPages: !isPaginated ? res : res?.total_pages,
 					loading: false,
 				});
 				console.log(res, "use fetch");
