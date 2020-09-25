5c8a275d96c4f452028c2cc4696482eaa3958939
Agustin Torra <torra.agustinignacio@gmail.com>
2020-09-25 19:07:57 -0300

    :sparkles: Add navbar menu

--- src/shared/components/navBar/NavBar.tsx
+++ src/shared/components/navBar/NavBar.tsx
@@ -5,6 +5,7 @@ import BurguerMenu from "../burguerMenu/BurguerMenu";
 import { colors } from "../../../browser/styles/colors";
 import { RootState } from "../../reducers/interface";
 import { useSelector } from "react-redux";
+import items from "../navMenu/items";
 
 type Props = {
 	history: RouteComponentProps["history"];
@@ -21,10 +22,14 @@ const NavBar: React.FC<Props> = ({ history }) => {
 			<Nav>
 				<Logo onClick={() => history.push("/")}>Moviar</Logo>
 				<div style={{ display: "flex" }}>
+					{items.map((item) => (
+						<p key={item.name} style={{ padding: ".5rem" }} onClick={() => history.push(item.path)}>
+							{item.name}
+						</p>
+					))}
 					<button
 						style={{
 							width: 100,
-							padding: ".5rem",
 							outline: "none",
 							backgroundColor: colors.primary,
 							color: "white",
---
+++ src/shared/components/navMenu/items.ts
@@ -0,0 +1,14 @@
+export default [
+	{
+		name: "Now playing",
+		path: "/now-playing",
+	},
+	{
+		name: "Popular",
+		path: "/popular",
+	},
+	{
+		name: "Upcoming",
+		path: "/upcoming",
+	},
+];
