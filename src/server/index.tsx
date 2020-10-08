363b01f4783749db7961ae287855186d1b0767ce
Agustin Torra <torra.agustinignacio@gmail.com>
2020-10-08 11:12:23 -0300

    Add process.env.PORT and remove commented code

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -33,43 +33,6 @@ async function handleRender(req, res) {
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-// app.get("*", (req, res, next) => {
-// 	const activeRoute: any = routes.find((route) => matchPath(req.url, route)) || {};
-
-// 	const promise = activeRoute.fetchInitialData
-// 		? activeRoute.fetchInitialData(req.params[0].split("/").pop())
-// 		: Promise.resolve();
-
-// 	promise
-// 		.then((data) => {
-// 			const markup = renderToString(
-// 				<Router location={req.url}>
-// 					<App />
-// 				</Router>
-// 			);
-
-// 			res.send(`
-// 				<!DOCTYPE html>
-// 				<html lang="en">
-// 					<head>
-// 						<meta charset="UTF-8">
-// 						<meta name="viewport" content="width=device-width, initial-scale=1.0">
-// 						<title>Moviar</title>
-// 						<script src="/bundle.js" defer></script>
-// 						<script>window.__INITIAL_DATA__=${serialize(data)}</script>
-// 						<link rel="stylesheet" type="text/css" href="/styles.css">
-// 						<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;900&display=swap" rel="stylesheet">
-// 					</head>
-
-// 					<body>
-// 						<div id="app">${markup}</div>
-// 					</body>
-// 				</html>
-//         `);
-// 		})
-// 		.catch(next);
-// });
-
-app.listen(3000, () => {
-	console.log("Server is listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Server is listening on port ${process.env.PORT ? process.env.PORT : 3000}`);
 });
